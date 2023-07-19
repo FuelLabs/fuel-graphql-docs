14d5e0b7e46455aea38ab0dc31816dc9edb36d86
sarahschwartz <58856580+sarahschwartz@users.noreply.github.com>
2023-07-19 16:01:47 -0600

    add component export test

--- tests/structure.test.ts
+++ tests/structure.test.ts
@@ -1,20 +1,21 @@
 import path from "path";
 import fs from "fs";
+import { execSync } from "child_process";
 
 describe("Is compatible with the docs hub", () => {
-  const docsFolderPath = path.join(__dirname, "../docs");
+  const DOCS_DIRECTORY = path.join(__dirname, "../docs");
   const subfolders = fs
-    .readdirSync(docsFolderPath)
+    .readdirSync(DOCS_DIRECTORY)
     .filter((item) =>
-      fs.statSync(path.join(docsFolderPath, item)).isDirectory()
+      fs.statSync(path.join(DOCS_DIRECTORY, item)).isDirectory()
     );
 
   it("should not have nested subfolders", () => {
     const nestedSubfolders = subfolders.filter((subfolder) =>
       fs
-        .readdirSync(path.join(docsFolderPath, subfolder))
+        .readdirSync(path.join(DOCS_DIRECTORY, subfolder))
         .some((item) =>
-          fs.statSync(path.join(docsFolderPath, subfolder, item)).isDirectory()
+          fs.statSync(path.join(DOCS_DIRECTORY, subfolder, item)).isDirectory()
         )
     );
     expect(nestedSubfolders).toHaveLength(0);
@@ -36,5 +37,13 @@ describe("Is compatible with the docs hub", () => {
     const compJSON = JSON.parse(compFile);
     expect(Array.isArray(compJSON.folders)).toBe(true);
     expect(Array.isArray(compJSON.ignore)).toBe(true);
+  });
+
+  it("should have an up-to-date component-exports file", () => {
+    const compPath = path.join(process.cwd(), "./src/component-exports.ts");
+    const existingContent = fs.readFileSync(compPath, 'utf8');
+    execSync('npm run export:components');
+    const updatedContent = fs.readFileSync(compPath, 'utf8');
+    expect(updatedContent).toEqual(existingContent);
   });
 });
