62725c7d70edbfd99ebc20c712d803c62772cb77
sarahschwartz <58856580+sarahschwartz@users.noreply.github.com>
2023-07-20 11:29:40 -0600

    remove script

--- tests/structure.test.ts
+++ tests/structure.test.ts
@@ -1,14 +1,14 @@
 import path from "path";
 import fs from "fs";
-import { execSync } from "child_process";
+
+const DOCS_DIRECTORY = path.join(__dirname, "../docs");
+const COMP_CONFIG_PATH = path.join(process.cwd(), "./src/components.json");
 
 describe("Is compatible with the docs hub", () => {
-  const DOCS_DIRECTORY = path.join(__dirname, "../docs");
-  const subfolders = fs
-    .readdirSync(DOCS_DIRECTORY)
-    .filter((item) =>
-      fs.statSync(path.join(DOCS_DIRECTORY, item)).isDirectory()
-    );
+  const files = fs.readdirSync(DOCS_DIRECTORY);
+  const subfolders = files.filter((item) =>
+    fs.statSync(path.join(DOCS_DIRECTORY, item)).isDirectory()
+  );
 
   it("should not have nested subfolders", () => {
     const nestedSubfolders = subfolders.filter((subfolder) =>
@@ -32,18 +32,58 @@ describe("Is compatible with the docs hub", () => {
   });
 
   it("should have a components.json file", () => {
-    const compPath = path.join(process.cwd(), "./src/components.json");
-    const compFile = fs.readFileSync(compPath, "utf8");
+    const compFile = fs.readFileSync(COMP_CONFIG_PATH, "utf8");
     const compJSON = JSON.parse(compFile);
     expect(Array.isArray(compJSON.folders)).toBe(true);
     expect(Array.isArray(compJSON.ignore)).toBe(true);
   });
 
-  it("should have an up-to-date component-exports file", () => {
-    const compPath = path.join(process.cwd(), "./src/component-exports.ts");
-    const existingContent = fs.readFileSync(compPath, 'utf8');
-    execSync('npm run export:components');
-    const updatedContent = fs.readFileSync(compPath, 'utf8');
-    expect(updatedContent).toEqual(existingContent);
+  it("component files should have same name as component", () => {
+    files.forEach((filename) => {
+      const filepath = path.join(DOCS_DIRECTORY, filename);
+      if (fs.statSync(filepath).isDirectory()) {
+        const subFiles = fs.readdirSync(filepath);
+        subFiles.forEach((subFilename) => {
+          const subFilepath = path.join(filepath, subFilename);
+          checkFile(subFilepath)
+        });
+      } else {
+        checkFile(filepath);
+      }
+    });
   });
 });
+
+function checkFile(filepath: string) {
+  const file = fs.readFileSync(filepath, "utf8");
+  const components = getComponents(file);
+  const compFile = fs.readFileSync(COMP_CONFIG_PATH, "utf8");
+  const compJSON = JSON.parse(compFile);
+  components.forEach((comp) => {
+    if (!compJSON.ignore.includes(comp)) {
+      let actualCompPath = "";
+      for (let i = 0; i < compJSON.folders.length; i++) {
+        const path = `${compJSON.folders[i]}/${comp.includes(".") ? comp.split(".").pop()! : comp}`;
+        const actualPath = `${process.cwd()}${path}.tsx`;
+        if (fs.existsSync(actualPath)) {
+          actualCompPath = `..${path}`;
+          break;
+        }
+      }
+      expect(actualCompPath).not.toEqual("");
+    }
+  });
+}
+
+function getComponents(mdxContent: string) {
+  const regex = /<([A-Za-z0-9_.]+)[^>]*\/>/g;
+  const matches = mdxContent.match(regex);
+  if (!matches) {
+    return [];
+  }
+  const components = matches.map((match) => {
+    const componentName = match.replace(/<([A-Za-z0-9_.]+)[^>]*\/>/, "$1");
+    return componentName.trim();
+  });
+  return Array.from(new Set(components));
+}
